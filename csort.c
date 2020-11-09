2c3fab096758a6ceb855e14cfcdaf7d496545fa1
gdrayton001 <47512642+gdrayton001@users.noreply.github.com>
2020-11-08 22:55:42 -0600

    Create csort.c

---
+++ csort.c
@@ -0,0 +1,96 @@
+/* assert */
+#include <assert.h>
+
+/* EXIT_SUCCESS, rand */
+#include <stdlib.h>
+
+/* strtol */
+#include <stdio.h>
+
+#include <omp.h>
+
+static int csort(unsigned const k,
+      unsigned const n,
+      unsigned const * const in,
+      unsigned       * const out)
+{
+  	unsigned * const count = calloc(k + 1, sizeof(*count));
+  	if (NULL == count) {
+    		return -1;
+  	}
+
+  	#pragma omp parallel num_threads(n)
+ 	{
+		#pragma omp for
+  		for (unsigned i = 0; i < n; i++) {
+    			count[in[i]]++;
+  		}
+	}
+
+  	unsigned total = 0;
+  	for (unsigned i = 0; i <= k; i++) {
+    		unsigned const counti = count[i];
+    		count[i] = total;
+    		total += counti;
+  	}
+
+	#pragma omp parallel num_threads(n)
+	{
+		#pragma omp for
+  		for (unsigned i = 0; i < n; i++) {
+    			out[count[in[i]]] = in[i];
+    			count[in[i]]++;
+  		}
+	}
+
+  	free(count);
+
+  	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	double const tstart = omp_get_wtime();
+	
+  	/* Get array size from command line */
+  	unsigned n = strtol(argv[1], NULL, 10);
+
+  	/* Get key size from command line */
+  	unsigned k = strtol(argv[2], NULL, 10);
+
+  	/* Allocate memory */
+  	unsigned * const a = malloc(n * sizeof(*a));
+  	unsigned * const b = malloc(n * sizeof(*b));
+
+  	/* Populate with random values */
+  	for (unsigned i = 0; i < n; i++) {
+    		a[i] = rand() % (1u << k);
+  	}
+
+  	/* Sort array */
+  	int const ret = csort(1u << k, n, a, b);
+  	assert(0 == ret);
+
+  	/* Validate sorted array */
+  	for (unsigned i = 1; i < n; i++) {
+    		assert(b[i] >= b[i - 1]);
+  	}
+
+	//for (unsigned i = 0; i < n; i++) {
+		//printf("%d ", a[i]);
+	//}
+	//printf("\n");
+
+	//for (unsigned i = 0; i < n; i++) {
+		//printf("%d ", b[i]);
+	//}
+	//printf("\n");
+
+  	/* Free memory */
+  	free(a);
+  	free(b);
+
+	double const tend = omp_get_wtime();
+	//printf("elapsed time: %lf sec.\n", tend-tstart);
+
+  	return EXIT_SUCCESS;
+}
